2c85cbb985f5107483735bec3c4a2a5c9eb98263
dam-brz <dam-brz@wp.pl>
2021-08-25 11:25:52 +0200

    Create service method to find employee by id

--- src/main/java/com/dambrz/emscrud/service/EmployeeService.java
+++ src/main/java/com/dambrz/emscrud/service/EmployeeService.java
@@ -5,6 +5,7 @@ import com.dambrz.emscrud.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
@@ -21,5 +22,9 @@ public class EmployeeService {
 
     public void saveEmployee(Employee employee) {
         this.employeeRepository.save(employee);
+    }
+
+    public Optional<Employee> getEmployeeById(long id) {
+        return employeeRepository.findById(id);
     }
 }
