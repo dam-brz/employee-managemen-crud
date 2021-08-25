11fb7e728b758db5ca65fd58cfa251795354512e
dam-brz <dam-brz@wp.pl>
2021-08-25 11:28:37 +0200

    Create mapping for return employee update template

--- src/main/java/com/dambrz/emscrud/controller/EmployeeController.java
+++ src/main/java/com/dambrz/emscrud/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -38,5 +39,12 @@ public class EmployeeController {
 
         employeeService.saveEmployee(employee);
         return "redirect:/";
+    }
+
+    @GetMapping("/showFormForUpdate/{id}")
+    public String showFormForUpdate(@PathVariable(value = "id") long id, Model model) {
+
+        model.addAttribute("employee", employeeService.getEmployeeById(id));
+        return "update-employee";
     }
 }
